c32a37e7fa09a120a294e33027af30332f693771
MINSEOK OH <oms0803@gmail.com>
2020-01-08 03:32:03 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+# Source : https://financedata.github.io/posts/python_news_cloud_text.html
+
+import pandas as pd
+
+import feedparser
+import newspaper
+from konlpy.tag import Kkma
+from collections import Counter
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+
+def draw_wordcloud_from_rss(rss_link):
+    #  feedparser, newspaper: RSS를 통해 뉴스의 본문을 수집
+    feeds = feedparser.parse(rss_link)
+    links = [entry['link'] for entry in feeds['entries']]
+
+    news_text = ''
+    for link in links:
+        article = newspaper.Article(link, language='ko')
+        article.download()
+        article.parse()
+        news_text += article.text
+
+    # konlpy, Mecab: 형태소 분석을 통해 본문에서 명사추출, 1글자는 단어는 삭제
+    engine = Kkma()
+    nouns = engine.nouns(news_text)
+    nouns = [n for n in nouns if len(n) > 1]
+
+    # Counter: 단어수 세기, 가장 많이 등장한 단어(명사) 40개
+    count = Counter(nouns)
+    tags = count.most_common(40)
+
+    # WordCloud, matplotlib: 단어 구름 그리기
+    font_path = '/Users/minseok/Downloads/Nanum_Myeongjo/NanumMyeongjo-Bold.ttf'
+    wc = WordCloud(font_path=font_path, background_color='white', width=800, height=600)
+    cloud = wc.generate_from_frequencies(dict(tags))
+    plt.figure(figsize=(10, 8))
+    plt.axis('off')
+    plt.imshow(cloud)
+    plt.show()
+
+
+# 경향신문 경제뉴스 RSS
+rss_link = 'http://www.khan.co.kr/rss/rssdata/economy.xml'
+draw_wordcloud_from_rss(rss_link)
